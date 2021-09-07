a2b6af7092ff59b87a7d8d8b2aaf6d518f9a011c
asdrubalramirezb <46140948+asdrubalramirezb@users.noreply.github.com>
2021-09-06 21:12:31 -0500

    Initial commit

---
+++ Assets/FacaMask.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class FacaMask : MonoBehaviour
+{
+   
+    public GameObject faceMask;   
+    private List<AugmentedFace> _tempFaceMask = new List<AugmentedFace>();
+    // Update is called once per frame
+    void Update()
+    {
+        Session.GetTrackables<AugmentedFace>(_tempFaceMask, TrackableQueryFilter.All);
+        if (_tempFaceMask.Count == 0)
+        {
+            faceMask.SetActive(false);
+        }
+        else
+        {
+            faceMask.SetActive(true);
+        }
+
+    }
+}
